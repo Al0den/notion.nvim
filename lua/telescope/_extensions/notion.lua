db7b87e29cef13d37cff2aec62bb77d8f376a9c4
Aloden <111601320+Al0den@users.noreply.github.com>
2023-04-30 19:01:59 +0100

    Update notion.lua

--- lua/telescope/_extensions/notion.lua
+++ lua/telescope/_extensions/notion.lua
@@ -1,6 +1,5 @@
 return require("telescope").register_extension {
     setup = function(ext_config, config)
-        -- access extension config and user config
     end,
     exports = {
         notion = require("notion.telescope").openMenu({})
